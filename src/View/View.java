2d96441467e0caf90c430f514d5035858137efd1
AdonayXX <emdiazdu@est.utn.ac.cr>
2023-11-28 23:28:50 -0600

    Clase controladora oficial y view
    Se crea la interfaz view, y la clase controladora

---
+++ src/View/View.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author wendy
+ */
+public interface View <Class> {
+    public void display(Class obj);
+    public void displayErrorMessage(String message);
+    public void displayMessage(String message);
+    public boolean displayConfirmMessage(String message);
+    
+}
